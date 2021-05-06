0dea9555481be9b84798ae4725d4c57d75c81de9
MIXMARLEON <45977278+MIXMARLEON@users.noreply.github.com>
2021-05-06 17:12:19 +0400

    Update main.py
    Added description for the program

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+
+This program is designed to test a convolutional neural network
+based on VGG16. It allows you to enter a handwritten number or open
+a file on your computer. After processing, the result is displayed
+in the form of a processed image and a bar chart, which shows
+the degree of "confidence" of the network that the image corresponds
+to each of the 10 digits.
+
+"""
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
