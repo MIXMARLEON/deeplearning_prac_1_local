76fcbf36d800ace7c5be92773b35ba63d5fa9f8f
MIXMARLEON <45977278+MIXMARLEON@users.noreply.github.com>
2021-05-06 17:22:51 +0400

    Indentation
    Changed indentation of description

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 """
 
 This program is designed to test a convolutional neural network
-based on VGG16. It allows you to enter a handwritten number or open
-a file on your computer. After processing, the result is displayed
-in the form of a processed image and a bar chart, which shows
-the degree of "confidence" of the network that the image corresponds
-to each of the 10 digits.
+based on VGG16.It allows you to enter a handwritten number or
+open a file on your computer. After processing, the result
+is displayed in the form of a processed image and a bar chart,
+which shows the degree of "confidence" of the network
+that the image corresponds to each of the 10 digits.
 
 """
 import os
